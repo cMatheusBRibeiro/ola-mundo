69333e438454a6234accb432cf80a9bc5af82794
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-09-19 08:53:53 -0300

    #FEAT: Adicionado componente de MenuLink e detalhe de rota selecionada

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import styles from "./Menu.module.css";
+import MenuLink from "../MenuLink";
 
 export default function Menu() {
     return (
         <header>
             <nav className={styles.navegacao}>
-                <Link className={styles.link} to="/">Início</Link>
-                <Link className={styles.link} to="/sobre-mim">Sobre Mim</Link>
+                <MenuLink to="/">Início</MenuLink>
+                <MenuLink to="/sobre-mim">Sobre Mim</MenuLink>
             </nav>
         </header>
     );
