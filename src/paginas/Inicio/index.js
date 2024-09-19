f11f96c38d341b124fdaadba0fe83d7b8f04ccb1
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-09-19 19:23:13 -0300

    #FEAT: Adicionado layout de página padrão

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,19 +1,15 @@
 import styles from "./Inicio.module.css";
-import Banner from "componentes/Banner";
 import Post from "componentes/Post";
 import posts from "json/posts.json";
 
 export default function Inicio() {
     return (
-        <main>
-            <Banner/>
-            <ul className={styles.posts}>
-                {posts.map((post) => (
-                    <li key={post.id}>
-                        <Post post={post} />
-                    </li>
-                ))}
-            </ul>
-        </main>
+        <ul className={styles.posts}>
+            {posts.map((post) => (
+                <li key={post.id}>
+                    <Post post={post} />
+                </li>
+            ))}
+        </ul>
     );
 }
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Inicio from './paginas/Inicio';
 import SobreMim from './paginas/SobreMim';
 import Menu from './componentes/Menu';
 import Rodape from 'componentes/Rodape';
+import PaginaPadrao from 'componentes/PaginaPadrao';
 
 function AppRoutes() {
   return (
@@ -10,8 +11,11 @@ function AppRoutes() {
       <Menu />
       
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sobre-mim" element={<SobreMim />} />
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobre-mim" element={<SobreMim />} />
+        </Route>
+        
         <Route path="*" element={<h1>Página não encontrada!</h1>} />
       </Routes>
 
