f3721a90713b85857c8ede6b76546cc619d534ad
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-09-19 18:20:00 -0300

    #FEAT: Adicionado Banner no projeto, assets e rota absoluta nos imports

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,3 +1,10 @@
+import Banner from "componentes/Banner";
+
 export default function SobreMim() {
-    return <h1>Sobre mim...</h1>;
+    return (
+        <main>
+            <Banner/>
+            <h1>Sobre mim...</h1>
+        </main>
+    );
 }
