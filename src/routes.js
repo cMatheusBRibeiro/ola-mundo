4fefb3b4da3bd73c496dffd372484ab18f6e9331
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-09-19 19:03:54 -0300

    #FEAT: Adicionado posts e rodapé

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Inicio from './paginas/Inicio';
 import SobreMim from './paginas/SobreMim';
 import Menu from './componentes/Menu';
+import Rodape from 'componentes/Rodape';
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Menu />
+      
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/sobre-mim" element={<SobreMim />} />
         <Route path="*" element={<h1>Página não encontrada!</h1>} />
       </Routes>
+
+      <Rodape />
     </BrowserRouter>
   );
 }
