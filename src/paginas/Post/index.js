4f2609f67e4e81dd60c1684fa9f1579e0c405fad
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-09-20 10:16:27 -0300

    #FEAT: Adicionado filtro e carregamento do post

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,7 +1,19 @@
 import { useParams } from "react-router-dom";
+import posts from "json/posts.json";
+import PostModelo from "componentes/PostModelo";
 
 export default function Post() {
     const parametros = useParams();
+
+    const idPost = Number(parametros.id);
+    const post = posts.find((post) => post.id === idPost);
     
-    return <h1>Post {parametros.id}</h1>;
+    return (
+        <PostModelo
+            fotoCapa={`/assets/posts/${post.id}/capa.png`}
+            titulo={post.titulo}
+        >
+            Texto...
+        </PostModelo>
+    );
 }
