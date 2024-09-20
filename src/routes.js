11f697bb9e1417a6df63d1ac7e1db2500a2fc2d8
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-09-20 11:19:39 -0300

    #FEAT: Adicionado página NaoEncontrada quando não encontrar o post

--- src/routes.js
+++ src/routes.js
@@ -16,9 +16,9 @@ function AppRoutes() {
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicio />} />
           <Route path="sobre-mim" element={<SobreMim />} />
-          <Route path="posts/:id" element={<Post />} />
         </Route>
 
+        <Route path="/posts/:id" element={<Post />} />
         <Route path="*" element={<NaoEncontrada />} />
       </Routes>
 
